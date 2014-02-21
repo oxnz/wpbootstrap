bfcfcb95eb27b138fbe44e9781784d68188f547b
oxnz <yunxinyi@gmail.com>
2014-02-22 07:02:38 +0800

    refine footer

--- footer.php
+++ footer.php
@@ -13,8 +13,17 @@
 			</ul>
 		</div>
 		<div class="footer-copyright">
-			<p>Copyright &copy; 2011-2014 oxnz, All rights reserved.</p>
-			<p><a href="#">www.baidu.com</a></p>
+			<p class="pull-left">&copy; 2011-2014 oxnz. <i>Oxnz Web Design.</i>
+				<small><a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyfourteen' ) ); ?>"><?php printf( __( 'Powered by %s'), 'WordPress' ); ?></a></small>
+			</p>
+			<p class="pull-right">
+<a href="http://validator.w3.org/check/referer" title="This page validates as XHTML 1.0 Transitional">Valid XHTML</a>
+|
+<a href="http://jigsaw.w3.org/css-validator/check/referer" title="This page validates as CSS">Valid CSS</a>
+</a>
+			</p>
+			<p style="clear: both">Goinnovate cookies policy - To give you the best possible experience, this site uses cookies.</p>
+			<p>Using goinnvoate.co.uk means you agree to our use of cookies. We have published a new Cookies Policy. View cookies policy.</p>
 		</div>
 	</div><!-- #container -->
 </footer>

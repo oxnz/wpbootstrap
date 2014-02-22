6c612c874eb22f6b2d672ff918a69d0b02af39e3
oxnz <yunxinyi@gmail.com>
2014-02-22 10:11:36 +0800

    refine 404.png

--- 404.php
+++ 404.php
@@ -10,22 +10,19 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+		<header class="page-header">
+			<h1 class="page-title"><?php _e( 'Not Found', 'wpbootstrap' ); ?></h1>
+		</header>
 
-			<header class="page-header">
-				<h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
-			</header>
+		<div class="page-wrapper">
+			<div class="page-content">
+				<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'wpbootstrap' ); ?></h2>
+				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'wpbootstrap' ); ?></p>
 
-			<div class="page-wrapper">
-				<div class="page-content">
-					<h2><?php _e( 'This is somewhat embarrassing, isn&rsquo;t it?', 'twentythirteen' ); ?></h2>
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</div><!-- .page-wrapper -->
-
-		</div><!-- #content -->
+				<?php get_search_form(); ?>
+			</div><!-- .page-content -->
+		</div><!-- .page-wrapper -->
 	</div><!-- #primary -->
+<br><br>
 
 <?php get_footer(); ?>

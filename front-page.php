54d87ed4e52a25761a6b6c9f35fcd6368501cf14
oxnz <yunxinyi@gmail.com>
2014-02-27 18:08:32 +0800

    add front-page.php

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<ul class="projects well">
+	<li class="project">
+		<div class="title">
+			<h1>Projects</h1>
+		</div>
+	</li>
+	<li class="project">
+		<h1>Projects</h1>
+	</li>
+</ul>
+<?php get_footer(); ?>

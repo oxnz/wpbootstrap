a507166b2445a93f6fe91539a0108539f136d09e
oxnz <yunxinyi@gmail.com>
2014-02-28 12:32:10 +0800

    add about modal

--- footer.php
+++ footer.php
@@ -71,6 +71,23 @@
 		</div>
 	</div><!-- #container -->
 
+	<div class="modal fade" id="about">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h4 class="modal-title">About</h4>
+				</div><!-- #modal-header -->
+				<div class="modal-body">
+					<p>&copy; 2011-2014 Oxnz.</p>
+				</div><!-- #modal-body -->
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default btn-primary" data-dismiss="modal">OK</button>
+				</div>
+			</div>
+		</div>
+	</div><!-- #about -->
+
 	<div class="modal fade" id="contact" tabindex="-1" role="dialog" aria-hidden="true">
 	  <div class="modal-dialog">
 		<div class="modal-content">

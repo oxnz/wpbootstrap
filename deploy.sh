6dabdac63f2445030457538615daaa93c0d74cde
oxnz <yunxinyi@gmail.com>
2014-03-01 21:19:35 +0800

    add deploy script for convenient

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+rsync -avP -e ssh \
+	--delete \
+	--exclude "$0" \
+   	--exclude .git \
+	--exclude front-page.php \
+	./ \
+	yunxinyi@frs.sourceforge.net:/home/project-web/xinyi/htdocs/wp-content/themes/wpbootstrap/
+#rsync -avP -e ssh yunxinyi@frs.sourceforge.net:/home/project-web/xinyi/htdocs/ xinyi-bkp/

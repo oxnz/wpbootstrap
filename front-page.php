11b5d836e95f959807eeebefe334ce78be46b75c
oxnz <yunxinyi@gmail.com>
2014-03-03 17:15:18 +0800

    update test to box

--- front-page.php
+++ front-page.php
@@ -1,23 +1,10 @@
 <?php get_header(); ?>
 
-<div id="test" class="ser-pane">
-<div class="info-pane">
-	<div class="avatar">
-		<img alt="" src="http://0.gravatar.com/avatar/626f34dec21559c6a46ca536b97d186a?s=96&amp;d=http%3A%2F%2F0.gravatar.com%2Favatar%2Fad516503a11cd5ca435acc9bb6523536%3Fs%3D96&amp;r=G" class="avatar avatar-96 photo" height="96" width="96">
-	</div>
-	<ul class="profile">
-		<div class="name">yunxinyi</div>
-		<div class="email">yunxinyi@gmail.com</div>
-		<div class="admin"><a href="#">Dashboard</a>
-			- <a href="#">Account</a></div>
-		<div class="profile-link">view profile</div>
-	</ul>
+<div class="test">
+<div class="container">
+	<div>xx</div>
+	<div>yy</div>
 </div>
-	<div class="control-pane">
-		<a href="#" class="btn btn-primary">Logout</a>
-		<a href="#" class="btn btn-primary">Logout</a>
-		<span class="justify"></span>
-	</div>
 </div>
 
 <?php get_footer(); ?>

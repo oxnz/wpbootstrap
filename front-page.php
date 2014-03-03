c4ed42cfc3ad6de03d66128d42f0ed74397c1155
oxnz <yunxinyi@gmail.com>
2014-03-03 18:01:11 +0800

    drop test style

--- front-page.php
+++ front-page.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<div class="test">
-<div class="container">
-	<div>xx</div>
-	<div>yy</div>
-</div>
-</div>
+<nav class="navigation post-navigation" role="navigation">
+	<div class="nav-links">
+		<a href="http://localhost/~oxnz/test-post/" rel="prev"><i class="fa fa-arrow-circle-o-left"></i> test post</a>
+		<a href="http://localhost/~oxnz/mysql/" rel="next">mysql <i class="fa fa-arrow-circle-o-right"></i></a>
+	</div><!-- .nav-links -->
+</nav>
 
 <?php get_footer(); ?>

c8f24c18512da02c0211817a90b028d7756a6446
oxnz <yunxinyi@gmail.com>
2014-03-06 16:28:06 +0800

    update front-page test case

--- front-page.php
+++ front-page.php
@@ -1,45 +1,42 @@
 <?php get_header(); ?>
 
-<h2>Explore my portfolio by project type</h2>
-
+</div><!-- .container -->
 <div class="portfolio">
-	<div class="view view-slide">
-		<img src="<?php echo get_template_directory_uri(); ?>/img/1.jpg">
-		<div class="mask">
-			<h2 class="title">This is the title</h2>
-			<p class="intro">avatar of the rocket since; which is usedaded at early time, used in this blog and other resources as avatar.</p>
-			<a href="#" class="more">Learn More &raquo;</a>
-		</div>
-	</div><!-- .cover -->
-	<div class="view view-slide">
-		<img src="<?php echo get_template_directory_uri(); ?>/img/1.jpg">
-		<div class="mask">
-			<h2 class="title">This is the title</h2>
-			<p class="intro">avatar of the rocket since; which is usedaded at early time, used in this blog and other resources as avatar.</p>
-			<a href="#" class="more">Learn More &raquo;</a>
-		</div>
-	</div><!-- .cover -->
-	<div class="view view-slide">
-		<img src="<?php echo get_template_directory_uri(); ?>/img/1.jpg">
-		<div class="mask">
-			<h2 class="title">This is the title</h2>
-			<p class="intro">avatar of the rocket since; which is usedaded at early time, used in this blog and other resources as avatar.</p>
-			<a href="#" class="more">Learn More &raquo;</a>
-		</div>
-	</div><!-- .cover -->
-</div>
-
-<div class="portrait">
-	<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
-	<div class="mask">
-		<a class="more" href="#">View Profile</a>
+	<div class="header">
+		<h2 class="title container">Explore my portfolio by project type</h2>
 	</div>
-	<span class="bgline"></span>
-</div>
-<div class="intro">
-<h1>The portfolio of Zhang Pan.</h1>
-<p>I design interfaces, mobile apps and craft things for the web. Take a look at some of the things I've worked on.</p>
-</div>
+	<div class="content container">
+		<div class="view view-slide">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/dcc.png">
+			<div class="mask">
+				<h2 class="title">This is the title</h2>
+				<p class="intro">avatar of the rocket since; which is usedaded at early time, used in this blog and other resources as avatar.</p>
+				<a href="#" class="more">Learn More &raquo;</a>
+			</div>
+		</div><!-- .cover -->
+		<div class="view view-slide">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/fs.png">
+			<div class="mask">
+				<h2 class="title">This is the title</h2>
+				<p class="intro">avatar of the rocket since; which is usedaded at early time, used in this blog and other resources as avatar.</p>
+				<a href="#" class="more">Learn More &raquo;</a>
+			</div>
+		</div><!-- .cover -->
+		<div class="view view-slide">
+			<img src="<?php echo get_template_directory_uri(); ?>/img/wpb.png">
+			<div class="mask">
+				<h2 class="title">This is the title</h2>
+				<p class="intro">avatar of the rocket since; which is usedaded at early time, used in this blog and other resources as avatar.</p>
+				<a href="#" class="more">Learn More &raquo;</a>
+			</div>
+		</div><!-- .cover -->
+	</div><!-- .content -->
+	<div class="footer">
+		<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
+		<h3>I am a design firm with a passion to make life beautiful</h3>
+		<a href="#">See More Works &raquo;</a>
+	</div>
+</div><!-- .portfolio -->
 
-
+<div class="container">
 <?php get_footer(); ?>

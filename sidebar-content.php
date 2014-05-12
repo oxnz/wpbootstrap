3b06200b61a14f13df9052a12ebd63fceb5ed629
oxnz <yunxinyi@gmail.com>
2014-05-12 20:46:05 +0800

    add sidebar-{content,footer}.php

---
+++ sidebar-content.php
@@ -0,0 +1,10 @@
+<?php
+
+if ( ! is_active_sidebar( 'sidebar-2' ) ) {
+	return;
+}
+?>
+
+<div id="content-sidebar" class="content-sidebar widget-area" role="complementary">
+	<?php dynamic_sidebar( 'sidebar-2' ); ?>
+</div>
---
+++ sidebar-footer.php
@@ -0,0 +1,12 @@
+<?php
+
+if ( ! is_active_sidebar( 'sidebar-3' ) ) {
+	return;
+}
+?>
+
+<div id="supplementary">
+	<div id="footer-sidebar" class="footer-sidebar widget-area" role="complementary">
+		<?php dynamic_sidebar( 'sidebar-3' ); ?>
+	</div>
+</div>

8097e75c24fbc243e82b29484d7d9466ce0c1458
oxnz <yunxinyi@gmail.com>
2014-05-14 16:53:24 +0800

    add NZWeather_Widget

--- inc/widgets.php
+++ inc/widgets.php
@@ -95,4 +95,157 @@ class Follow_Me_Widget extends WP_Widget {
 					    })();
 		</script>
  */
+
+
+class NZWeather_Widget extends WP_Widget {
+	function __construct() {
+		$widget_ops = array('classname' => 'nzweather',
+			'description' => __('A beautiful weather widget'));
+		parent::__construct('nzweather', __('NZWeather'), $widget_ops);
+	}
+
+	function widget($args, $instance) {
+		extract($args);
+		$title = empty($instance['title']) ? $instance['title'] : __('NZWeather');
+		$title = apply_filters('widget_title', $title, $instance, $this->id_base);
+		$location = (!empty($instance['location'])) ? $instance['location'] : __('Livepool, UK');
+		$wiconurl = get_stylesheet_directory_uri() . '/img/weather-icons/';
+		$today = '
+<div class="today">
+	<div class="info">
+		<div class="temp"><span id="tempnum">29</span>&#176;</div>
+		<div class="loc">' . $location . '</div>
+	</div>
+	<img class="wicon" id="wicon" src="' . $wiconurl . 'na.png">
+</div>';
+	$predcnt = 3; // predict 3 days
+	for ($i = 1; $i <= $predcnt; ++$i) {
+		$predict .= '
+<div class="preditem">
+	<div class="dayname">' . ((1 == $i) ? 'Tomorrow' : date('l', strtotime('+' . $i . 'days'))) . '</div>
+	<div class="temp"><span class="tempnum">29</span>&#176;</div>
+	<div class="wicon"><img class="wicon" src="' . $wiconurl . 'na.png"></div>
+</div>';
+	}
 ?>
+<script type="text/javascript">
+jQuery(document).ready(function ($) {
+	function gettemp(temp, precision) {
+		return (temp-273.15).toFixed(precision);
+	}
+
+	function getwicon(weather) {
+		var baseurl = "<?php echo $wiconurl; ?>";
+		switch (weather) {
+		case "Rain":
+			return baseurl + "rain.png";
+		case "Clouds":
+			return baseurl + "cloud.png";
+		default:
+			console.log("no appropriate icon for weather: " + weather);
+			return baseurl + "na.png";
+		}
+	}
+	/*
+	 * thank you for http://api.openweathermap.org
+	 */
+	$.getJSON( // today
+		url = "http://api.openweathermap.org/data/2.5/weather",
+		data = { q: "<?php echo $location; ?>" },
+		success = function(result, stat, xhr) {
+			$('.today span#tempnum')[0].textContent = gettemp(result.main.temp, 0);
+			$('.today img.wicon')[0].src = getwicon(result.weather[0].main);
+		}
+	);
+	$.getJSON( // forcast
+		url = "http://api.openweathermap.org/data/2.5/forecast/daily",
+		data = {
+			q:		"<?php echo $location; ?>",
+			cnt:	"<?php echo $predcnt; ?>",
+			mode:	"json"
+		},
+		success = function(result, stat, xhr) {
+			var temps = $(".preditem .tempnum");
+			var wicons = $(".preditem img.wicon");
+			for (var i = 0; i < temps.length; ++i) {
+				temps[i].textContent = gettemp(result.list[i].temp.eve, 2);
+				wicons[i].src = getwicon(result.list[i].weather[0].main);
+			}
+	});
+});
+</script>
+<style type="text/css">
+.nzweather .today {
+	width: 100%;
+	display: -webkit-box;
+	display: -moz-box;
+	display: box;
+	-webkit-box-pack: justify;
+	-moz-box-pack: justify;
+	box-pack: justify;
+}
+
+.nzweather .today .info {
+	-webkit-box-flex: 1;
+}
+
+.nzweather .today .info .temp {
+	font-size: 500%;
+}
+
+.nzweather .today .info .loc {
+	padding-top: 40px;
+}
+
+.nzweather .today .wicon {
+	padding-top: 20px;
+	width: 128px;
+}
+
+.nzweather .preditem {
+	display: -webkit-box;
+	-webkit-box-pack: justify;
+	-webkit-box-align: center;
+	font-size: 140%;
+}
+
+.nzweather .preditem > .dayname {
+	-webkit-box-flex: 1;
+}
+
+.nzweather .preditem > .temp {
+	padding-right: 30px;
+}
+
+.nzweather .preditem > .wicon img {
+	width: 40px;
+}
+
+</style>
+<?php
+		echo $before_widget . $before_title . $today . $after_title . $predict . $after_widget;
+	}
+
+	function update($new_instance, $old_instance) {
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['location'] = $new_instance['location'];
+		return $instance;
+	}
+
+	function form($instance) {
+		$title = isset($instance['title']) ? esc_attr($instance['title']) : ' ';
+		$location = isset($instance['location']) ?
+			esc_attr($instance['location']) : 'Livepool, UK';
+?>
+<p>
+	<label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+	<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name = "<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+</p>
+<p>
+	<label for="<?php echo $this->get_field_id('location'); ?>"><?php _e('Location:'); ?></label>
+	<input class="widefat" id="<?php echo $this->get_field_id('location'); ?>" name = "<?php echo $this->get_field_name('location'); ?>" type="text" value="<?php echo $location; ?>" />
+</p>
+<?php
+	}
+}

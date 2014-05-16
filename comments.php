1f24a87dfac816298f6e4fcf710667153246aec3
oxnz <yunxinyi@gmail.com>
2014-05-16 17:58:41 +0800

    relayout comments list

--- comments.php
+++ comments.php
@@ -35,9 +35,46 @@ if ( post_password_required() ) {
 	<ol class="comment-list">
 		<?php
 			wp_list_comments( array(
-				'style'      => 'ol',
-				'short_ping' => true,
+				'style'		=> 'ol',
 				'avatar_size'=> 80,
+				'callback'	 => function($comment, $args, $depth) {
+					if ('div' == $args['style']) {
+						$tag = 'div';
+						$add_below = 'comment';
+					} else {
+						$tag = 'li';
+						$add_below = 'div-comment';
+					}
+?>
+	<<?php echo $tag; ?> <?php comment_class(empty($args['has_children']) ? '' : 'parent') ?> id="comment-<?php comment_ID() ?>">
+
+<?php if ('div' != $args['style']) : ?>
+<div id="div-comment-<?php comment_ID() ?>" class="comment-body">
+<?php endif; ?>
+
+<div class="comment-author vcard">
+<?php if (0 != $args['avatar_size']) echo get_avatar($comment, $args['avatar_size']); ?>
+</div>
+
+<?php if ('0' == $comment->comment_approved) : ?>
+<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.'); ?></em>
+<?php endif; ?>
+
+<div class="comment-quote">
+	<div class="comment-header">
+		<span><?php printf(__('<cite class="fn">%s</cite> <span class="says">says:</span>'), get_comment_author_link()); ?></span>
+		<span class="meta"><a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s at %2$s'), get_comment_date(), get_comment_time()); ?></a><?php edit_comment_link(__('(Edit)'), ' ', ''); ?></span>
+	</div>
+	<?php comment_text(); ?>
+	<div class="reply"><?php comment_reply_link(array_merge($args, array(
+		'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))); ?>
+	</div>
+</div>
+<?php if ('div' != $args['style']) : ?>
+</div>
+<?php endif; ?>
+<?php
+				}
 			) );
 		?>
 	</ol><!-- .comment-list -->

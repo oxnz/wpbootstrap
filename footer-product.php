d043b801f9ee8f20d2062998ab9f6a04f1f723ac
oxnz <yunxinyi@gmail.com>
2014-05-17 23:16:10 +0800

    add product-footer

---
+++ footer-product.php
@@ -0,0 +1,35 @@
+<?php
+?>
+
+<footer class="product-footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4 colume">
+				<section>
+					Made with <i class="fa fa-heart"></i> by Oxnz.
+				</section>
+				<ul class="addthis_toolbox addthis_default_style">
+
+				              <li><a class="addthis_button_facebook_like"></a></li>
+							                <li><a class="addthis_button_tweet"></a></li>
+
+											            </ul>
+			</div>
+			<div class="col-md-4 colume">
+				<section>
+					Be sure to read <a href="#copyright">Terms</a> and <a href="#privacy">Privacy Policy</a>.
+			    </section>
+				<ul class="social-links">
+					<li><a class="fa fa-twitter" href="#fakelinks"></a></li>
+					<li><a class="fa fa-facebook" href="#fakelinks"></a></li>
+					<li><a class="fa fa-pinterest" href="#fakelinks"></a></li>
+				</ul>
+			</div>
+			<div class="col-md-4 colume">
+				<section>
+					  <strong>Errpro.com</strong> <br>Luo-jia-shan · Wuchang · Wuhan<br/>email: yunxinyi{@}gmail.com
+				  </section>
+			</div>
+		</div>
+	</div>
+</footer>

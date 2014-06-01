06129ce501d922326cdb046dd08275de95ea4667
oxnz <yunxinyi@gmail.com>
2014-06-01 11:52:07 +0800

    move google fonts from header.php to style.css

--- header.php
+++ header.php
@@ -25,7 +25,6 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js"></script>
 	<![endif]-->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400' rel='stylesheet' type='text/css'>
 	<link href="<?php echo site_url(); ?>/favicon.png"
 		rel="shortcut icon">
 	<link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">

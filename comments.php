cbf11bf77dd1a15d8737f0474f7c4be1e22513aa
oxnz <yunxinyi@gmail.com>
2014-06-07 02:11:15 +0800

    custom fields of comment_form

--- comments.php
+++ comments.php
@@ -20,30 +20,15 @@ if ( post_password_required() ) {
 
 <div id="comments" class="comments-area panel panel-default">
 	<?php if ( have_comments() ) : ?>
-<<<<<<< HEAD
 	<div class="panel-heading">
 		<h2 class="comments-title"><i class="fa fa-comments-o"></i>
-=======
-	<div class="panel-heading"><h2 class="comments-title">
->>>>>>> 9743fde1648cf706357b8f7eaa9057fc7fecea3b
 		<?php
 			printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'wpbootstrap' ),
 				number_format_i18n( get_comments_number() ), get_the_title() );
 		?>
-<<<<<<< HEAD
 		</h2>
 	</div>
 	<div class="panel-body">
-=======
-	</h2></div><!--/panel-heading-->
-	<div class="panel-body">
-	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : ?>
-	<ul class="pager">
-		<li class="previous"><?php previous_comments_link(__( '&larr; Older Comments')); ?></li>
-		<li class="next"><?php next_comments_link(__( 'Newer Comments &rarr;')); ?></li>
-	</ul>
-	<?php endif; // Check for comment navigation. ?>
->>>>>>> 9743fde1648cf706357b8f7eaa9057fc7fecea3b
 	<ol class="comment-list">
 		<?php
 			wp_list_comments( array(
@@ -71,20 +56,31 @@ if ( post_password_required() ) {
 	</div>
 	<div class="comment-meta">
 		<?php printf(__('<cite class="author">%s</cite>'), get_comment_author_link()); ?>
-					<br>
-	<a href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s at %2$s'), get_comment_date(), get_comment_time()); ?></a>
+		<br>
+		<a class="timestamp" href="<?php echo htmlspecialchars(get_comment_link($comment->comment_ID)); ?>"><?php printf(__('%1$s at %2$s'), get_comment_date(), get_comment_time()); ?></a>
 	</div>
 </div>
 
-<blockquote class="comment-content">
 <?php if ('0' == $comment->comment_approved) : ?>
-<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.'); ?></em>
+	<div class="comment-info alert alert-info alert-dismissable">
+		<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+		<i class="fa fa-info-circle"></i>
+		<em class="comment-awaiting-moderation"><?php _e('Your comment is awaiting moderation.'); ?></em>
+	</div>
 <?php endif; ?>
+
+<blockquote class="comment-content">
 <?php comment_text(); ?>
 </blockquote>
 
 	<ol class="comment-footer">
 		<?php edit_comment_link(__('<i class="fa fa-edit"></i> Edit'), '<li> ', '</li>'); ?>
+<?php if (current_user_can('edit_post')) : ?>
+		<li><a href="<?php echo admin_url('comments.php?action=cdc&dt=spam&c='
+			. get_comment_ID()); ?>"><i class="fa fa-trash-o"></i> Spam</a></li>
+		<li><a href="<?php echo admin_url('comments.php?action=cdc&c='
+			. get_comment_ID()); ?>"><i class="fa fa-times"></i> Delete</a></li>
+<?php endif; ?>
 		<li><?php comment_reply_link(array_merge($args, array(
 			'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth']))); ?></li>
 		<li><a href="#"><i class="fa fa-share"></i> Share</a></li>
@@ -92,7 +88,7 @@ if ( post_password_required() ) {
 		<li><a href="#"><i class="fa fa-flag"></i> Report</a></li>
 	</ol><!--/comment-footer-->
 <?php if ('div' != $args['style']) : ?>
-</div>
+</div><!--/comment-body-->
 <?php endif; ?>
 <?php
 				}
@@ -123,10 +119,42 @@ if ( post_password_required() ) {
 		'comment_field' =>  '<p class="comment-form-comment">' .
 		'<textarea class="form-control" id="comment" name="comment" rows="6" aria-required="true"></textarea></p>',
 		'label_submit' => __('Submit Comment'),
+		'fields'	=> apply_filters('comment_form_default_fields', array(
+			'author' => '<p><div class="input-group">'
+			. '<span class="input-group-addon">'
+			. '<i class="fa fa-fw fa-user"></i> '
+			//. __('Name', 'domainreference')
+			. '</span><input id="author" name="author" type="text" '
+			. 'value="' . esc_attr($commenter['comment_author']) . '"'
+			. $aria_req . ' class="form-control" placeholder="Username" />'
+			. ($req ? '<span class="input-group-addon required">'
+			. '<i class="fa fa-fw fa-circle"></i></span>' : '')
+			. '</div></p>'
+			,
+			'email' => '<p><div class="input-group">'
+			. '<span class="input-group-addon">'
+			. '<i class="fa fa-fw fa-envelope-o"></i> '
+			//. __('Email', 'domainreference')
+			. '</span><input id="email"'
+			. 'name="email" type="text" value="'
+			. esc_attr($commenter['comment_author_email']) . '"' . $aria_req
+			. 'class="form-control" placeholder="email" /><span class="'
+			. ($req ? 'required' : 'optional') . ' input-group-addon">'
+			. '<i class="fa fa-fw ' . ($req ? 'fa-circle' : 'fa-circle-thin')
+			. '"></i></span>'
+			. '</div></p>'
+			,
+			'url' => '<p><div class="input-group">'
+			. '<span class="input-group-addon">'
+			. '<i class="fa fa-fw fa-link"></i> '
+			//. __('URL', 'domainreference')
+			. '</span><input id="url" name="url" type="text" value="'
+			. esc_attr($commenter['comment_author_url']) . '" class="'
+			. 'form-control" placeholder="website url" /><span class="'
+			. 'optional input-group-addon"><i class="fa fa-fw fa-circle-thin">'
+			. '</i></span>'
+			. '</div></p>'
+		)),
 	)); ?>
-<<<<<<< HEAD
-=======
-	<?php //comment_form(); ?>
->>>>>>> 9743fde1648cf706357b8f7eaa9057fc7fecea3b
 	</div><!--/panel-body-->
 </div><!-- #comments -->

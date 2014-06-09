e6348122d0008fc2f978725219477956555f3c5b
oxnz <yunxinyi@gmail.com>
2014-06-09 22:54:33 +0800

    add lorem generator function:lipsum

--- functions.php
+++ functions.php
@@ -61,28 +61,28 @@ function wpbootstrap_widgets_init() {
 		'name'          => __( 'Primary Sidebar', 'wpbootstrap' ),
 		'id'            => 'sidebar-1',
 		'description'   => __( 'Additional sidebar that appears on the right.', 'wpbootstrap' ),
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<div class="widget-title">',
-		'after_title'   => '</div>',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s panel panel-default">',
+		'before_title'  => '<div class="widget-title panel-heading">',
+		'after_title'   => '</div><div class="widget-body panel-body">',
+		'after_widget'  => '</div></aside>',
 	) );
 	register_sidebar( array(
 		'name'          => __( 'Content Sidebar', 'wpbootstrap' ),
 		'id'            => 'sidebar-2',
 		'description'   => __( 'Main sidebar that appears on the left.', 'wpbootstrap' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s panel panel-primary">',
-		'after_widget'  => '</div></aside>',
 		'before_title'  => '<div class="widget-title panel-heading">',
 		'after_title'   => '</div><div class="widget-body panel-body">',
+		'after_widget'  => '</div></aside>',
 	) );
 	register_sidebar( array(
 		'name'          => __( 'Footer Widget Area', 'wpbootstrap' ),
 		'id'            => 'sidebar-3',
 		'description'   => __( 'Appears in the footer section of the site.', 'wpbootstrap' ),
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
 		'before_title'  => '<div class="widget-title">',
 		'after_title'   => '</div>',
+		'after_widget'  => '</aside>',
 	));
 }
 add_action( 'widgets_init', 'wpbootstrap_widgets_init' );
@@ -260,3 +260,47 @@ function index_content($content) {
 	return $content;
 }
 add_filter('the_content', index_content);
+
+/*
+add_filter('pre_comment_content', function($content) {
+	$pattern = '@<(pre|code)(.*?)>([\w\W]*?)</\1>@i';
+	return preg_replace_callback($pattern, function($m) {
+		return sprintf('<%s%s>%s</%s>', $m[1], $m[2],
+			htmlspecialchars($m[3]), $m[1]);
+	}, $content);
+}, '99', 2);
+*/
+
+/*
+ * Lorem Ipsum is a pseudo-Latin text used in web design, typography, layout,
+ * and printing in place of English to emphasise design elements over content.
+ *
+ * this function is a simple lipsum generator
+ */
+
+function lipsum($display=true) {
+	static $index = -1;
+	$data1 = <<<LOREM
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+LOREM;
+	$data2 = <<<LOREM
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+LOREM;
+	$data3 = <<<LOREM
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+LOREM;
+	$data4 = <<<LOREM
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+LOREM;
+	$data5 = <<<LOREM
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+LOREM;
+	$data = array($data1, $data2, $data3, $data4, $data5);
+	if (++$index >= count($data))
+		$index = -1;
+	if ($display) {
+		echo $data[$index];
+	} else {
+		return $data[$index];
+	}
+}

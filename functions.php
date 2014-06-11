0b8dbcac8dd36e8555e54d58edce650476d8b0a9
oxnz <yunxinyi@gmail.com>
2014-06-11 18:55:18 +0800

    refine functions, move style and script from header.php to functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
+require get_template_directory() . '/inc/nzutils.php';
+
 if ( ! function_exists( 'wpbootstrap_setup' ) ) :
 /**
  * setup wpbootstrap theme defaults and registers support for various wordpress
@@ -29,6 +31,11 @@ function wpbootstrap_setup() {
 	// Enable support for Post Thumbnails, and declare two sizes.
 	add_theme_support( 'post-thumbnails', array( 'post', 'page') );
 
+	// Enable support for post format
+	add_theme_support( 'post-formats', array('aside', 'gallery', 'link',
+		'image', 'quote', 'status', 'video', 'audio', 'chat')
+	);
+
 	// This theme uses wp_nav_menu() in two locations
 	register_nav_menus( array(
 		'primary'	=> __( 'Top primary menu', 'wpbootstrap' ),
@@ -43,6 +50,8 @@ function wpbootstrap_setup() {
 		'search_form', 'comment-form', 'comment-list', 'gallery', 'caption'
 	));
 
+	// This theme uses its own gallery styles.
+	add_filter( 'use_default_gallery_style', '__return_false' );
 }
 endif; // wpbootstrap_setup
 add_action( 'after_setup_theme', 'wpbootstrap_setup' );
@@ -56,6 +65,8 @@ function wpbootstrap_widgets_init() {
 	require get_template_directory() . '/inc/widgets.php';
 	register_widget( 'NZWeather_Widget' );
 	register_widget( 'NZProfile_Widget' );
+	register_widget( 'NZRecentPostsWidget' );
+	register_widget( 'NZRecentCommentsWidget' );
 
 	register_sidebar( array(
 		'name'          => __( 'Primary Sidebar', 'wpbootstrap' ),
@@ -88,24 +99,6 @@ function wpbootstrap_widgets_init() {
 add_action( 'widgets_init', 'wpbootstrap_widgets_init' );
 
 
-//remove_filter('the_content', 'wpautop');
-//remove_filter('the_excerpt', 'wpautop');
-//remove_filter('the_content', 'wptexturize');
-//remove_filter('the_excerpt', 'wptexturize');
-//remove_filter('comment_text', 'wptexturize');
-//remove_filter('the_title', 'wptexturize');
-
-/*
-if (function_exists('register_sidebar'))
-	register_sidebar(array(
-		'before_widget' => '',
-		'after_widget' => '',
-		'before_title' => '<h3>',
-		'after_title' => '</h3>',
-	));
-
- */
-
 /*
  * alter login style
  */
@@ -124,10 +117,6 @@ function wplogin_style() { ?>
 add_action('login_enqueue_scripts', 'wplogin_style');
 
 require get_template_directory() . '/inc/template-tags.php';
-
-add_filter('get_calendar', function($cal) {
-	return $cal;
-});
 
 /*
  * customer nav menu walker
@@ -261,46 +250,100 @@ function index_content($content) {
 }
 add_filter('the_content', index_content);
 
-/*
-add_filter('pre_comment_content', function($content) {
-	$pattern = '@<(pre|code)(.*?)>([\w\W]*?)</\1>@i';
-	return preg_replace_callback($pattern, function($m) {
-		return sprintf('<%s%s>%s</%s>', $m[1], $m[2],
-			htmlspecialchars($m[3]), $m[1]);
-	}, $content);
-}, '99', 2);
-*/
+add_filter( 'excerpt_more', function($more) {
+	return '<a href="' . get_permalink() . '" title="Read More">'
+		. $more . '</a>';
+});
+
+add_filter( 'post_thumbnail_size', function($size) {
+	return 'thumbnail';
+	return $size;
+});
 
 /*
- * Lorem Ipsum is a pseudo-Latin text used in web design, typography, layout,
- * and printing in place of English to emphasise design elements over content.
- *
- * this function is a simple lipsum generator
+ * change google fonts to 360 cdn
  */
 
-function lipsum($display=true) {
-	static $index = -1;
-	$data1 = <<<LOREM
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-LOREM;
-	$data2 = <<<LOREM
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-LOREM;
-	$data3 = <<<LOREM
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
-LOREM;
-	$data4 = <<<LOREM
-It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-LOREM;
-	$data5 = <<<LOREM
-There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
-LOREM;
-	$data = array($data1, $data2, $data3, $data4, $data5);
-	if (++$index >= count($data))
-		$index = -1;
-	if ($display) {
-		echo $data[$index];
-	} else {
-		return $data[$index];
+add_filter( 'style_loader_src', function($src) {
+	return preg_replace(
+		'@//fonts\.googleapis\.com/css\?family=Open\+Sans(.*)(&ver=.*)$@i',
+		'//fonts.useso.com/css?family=Open+Sans${1}${2}',
+		$src);
+}, 100, 2 );
+
+function wpbootstrap_scripts() {
+	wp_enqueue_style( 'open-sans',
+		'//fonts.useso.com/css?family=Open+Sans:300,400', array()
+	);
+	wp_enqueue_style( 'wordpress', get_template_directory_uri()
+		. '/wordpress.css', array()
+	);
+	wp_enqueue_style( 'bootstrap', 
+		'//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css',
+		array(), null
+	);
+	wp_enqueue_style( 'font-awesome',
+		'//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css',
+		array(), null
+	);
+	wp_enqueue_style( 'wpbootstrap-style', get_stylesheet_uri(),
+		array( 'bootstrap', 'font-awesome', 'open-sans', 'wordpress' )
+	);
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
 	}
+
+	wp_register_script('bootstrap',
+		'//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js',
+		array('jquery'), null, true
+	);
+	wp_enqueue_script('bootstrap');
+	wp_enqueue_script( 'wpbootstrap-script', get_template_directory_uri()
+		. '/wpbootstrap.js', array( 'jquery' ), '201406', true);
 }
+add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts' );
+
+/**
+ * Extend the default Wordpress body classes
+ */
+add_filter( 'body_class', function($classes) {
+	return $classes;
+} );
+
+/**
+ * Extend the default Wordpress post classes.
+ */
+add_filter( 'post_class', function($classes) {
+	$classes[] = 'panel panel-default';
+	return $classes;
+} );
+
+/*
+ * Create a nicely formatted and more specific title element text for output
+ * in head of document, based on current view.
+ *
+ * @since WPBootstrap 1.8
+ */
+add_filter( 'wp_title', function($title, $sep) {
+	global $paged, $page;
+
+	if ( is_feed() ) {
+		return $title;
+	}
+
+	// Add a page number if ncessary.
+	if ( $paged >= 2 || $page >= 2 ) {
+		$title = "$title $sep " . sprintf( __( 'Page %s', 'wpbootstrap' ),
+			max( $paged, $page ) );
+	}
+
+	return $title;
+}, 10, 2);
+
+//remove_filter('the_content', 'wpautop');
+//remove_filter('the_excerpt', 'wpautop');
+//remove_filter('the_content', 'wptexturize');
+//remove_filter('the_excerpt', 'wptexturize');
+//remove_filter('comment_text', 'wptexturize');
+//remove_filter('the_title', 'wptexturize');
